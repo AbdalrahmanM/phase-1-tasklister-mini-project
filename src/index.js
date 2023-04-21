81b69ef86250e6988caf03959e7662a6d1521664
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-04-21 22:06:26 +0300

    updatede some functios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+    // your code here
+    let form=document.querySelector('form')
+    form.addEventListener('submit',(e)=>{
+      e.preventDefault()
+      createT(e.target.new_task_description.value)
+      form.reset()
+    })
+  });
+  
+  function createT(tasks){
+    let li=document.createElement('li')
+    let btn=document.createElement('input')
+    btn.addEventListener('click',deleteTask)
+    li.textContent=`${tasks} `
+    li.appendChild(btn)
+    btn.setAttribute('type','submit')
+    btn.setAttribute('value','Delete The Task')
+    document.querySelector('#tasks').appendChild(li)
+  }
+
+  function deleteTask(e){
+    e.target.parentNode.remove()
+  }
